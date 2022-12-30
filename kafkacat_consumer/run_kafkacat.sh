ba3f6fa99de0c32c71b1b6816293246e733be9d1
Flavio Bizzarri <newfla@hotmail.it>
2022-12-30 19:02:25 +0100

    added proto support

--- kafkacat_consumer/run_kafkacat.sh
+++ kafkacat_consumer/run_kafkacat.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-kafkacat -b kafka:29092 -C -t $TOPIC -f 'Topic %t[%p], offset: %o, key: %k, payload: %S\n"' 
+if [ "$PROTO" = true ] ; then
+    while :
+    do
+        kafkacat  -b kafka:29092 -C -t $TOPIC  -o -1 -e -q -D "" | protoc --decode=sender.proto.KafkaMessage --proto_path=./ ./message.proto
+    done
+else
+    kafkacat -b kafka:29092 -C -t $TOPIC -f 'Topic %t[%p], offset: %o, key: %k, payload: %S\n"' 
+fi
--- scripts/build_consumer.sh
+++ scripts/build_consumer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ### build kafka consumer ###
 pushd ../kafkacat_consumer
+cp -r ../Rust-UDP-To-Kafka-Forwarding/tasks/src/proto/message.proto message.proto
 docker build -t kafkacat_consumer:latest . 
+rm message.proto
 popd
