b1182af7415a3ce43b3d9ded5b4e5e2f2e7c1b5c
flavio <newfla@hotmail.it>
2023-07-15 11:59:12 +0200

    sync update

--- scripts/build_forwarder.sh
+++ scripts/build_forwarder.sh
@@ -5,6 +5,7 @@ mkdir forwarder
 cp -R ../crab-kafka/src forwarder
 cp -R ../crab-kafka/examples forwarder
 cp ../crab-kafka/Cargo.toml forwarder
+cp ../crab-kafka/README.md forwarder
 docker build -t udpforwarder:latest . 
 rm -rf forwarder
 popd
